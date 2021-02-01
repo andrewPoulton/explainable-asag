ed98731badb94977fcccc2c147b2dbc27cd0ca5e
Sebas <sebas.eliens@gmail.com>
2021-02-01 16:47:11 -0300

    Load configs

---
+++ configs.py
@@ -0,0 +1,86 @@
+from warnings import warn
+from types import SimpleNamespace
+
+# global config variables
+TRAIN_BATCH_SIZE = 4
+ACCUMULATION_STEPS = 4
+LEARN_RATE = 1e-5
+EPOCHS = 24
+WARMUP_STEPS = 1024
+SEQUENCE_LENGTH = 512
+
+
+
+# default config (overwritten by CONFIGS)
+DEFAULT_CONFIG = {
+    "model_path" : "bert-base-uncased",
+    "train_batch_size" : TRAIN_BATCH_SIZE,
+    "accumulation_steps" : ACCUMULATION_STEPS,
+    "learn_rate" : LEARN_RATE,
+    "epochs" : EPOCHS,
+    "warmup_steps" : WARMUP_STEPS,
+    "sequence_length" : SEQUENCE_LENGTH
+}
+
+
+# configs for experiments
+CONFIGS = {
+## size variation
+    "bert-base": {
+        "model_path" : "bert-base-uncased",
+        },
+    "bert-large" : {
+        "model_path" : "bert-large-uncased",
+    },
+    "roberta-base" : {
+        "model_path" : "roberta-base-uncased",
+        },
+    "roberta-large" : {
+        "model_path" : "roberta-large-uncased",
+        },
+    "albert-base" : {
+        "model_path" : "albert-base-v2",
+        },
+    "albert-large" : {
+        "model_path" : "albert-large-v2",
+        },
+## distilled versions
+    "distilbert-base-uncased" : {
+        "model_path" : "distilbert-base-uncased",
+        },
+    "distilroberta" : {
+        "model_path" : "distilroberta-base",
+        },
+## question-answering pretraining
+    "distilbert-base-squad2" : {
+        "model_path" : "twmkn9/distilbert-base-uncased-squad2",
+        },
+    "roberta-base" : {
+        "model_path" : "roberta-base-squad2",
+        },
+    "distilroberta-base-squad2" : {
+        "model_path" : "twmkn9/distilroberta-base-squad2",
+        },
+    "bert-base-squad2" : {
+        "model_path" : "twmkn9/bert-base-uncased-squad2",
+        },
+    "albert-base-squad2" : {
+        "model_path" : "twmkn9/albert-base-v2-squad2",
+    },
+## sentence similarity pretraining
+    "roberta-large-stsb" : {
+        "model_path" : "sentence-transformers/ce-roberta-large-stsb",
+    },
+    "distilroberta-base-stsb" : {
+        "model_path" : "sentence-transformers/ce-distilroberta-base-stsb"
+    }
+}
+
+
+def load(experiment):
+    config = DEFAULT_CONFIG
+    try:
+        config.update(CONFIGS[experiment])
+    except  AttributeError:
+        warn(f"Did not choose experiment from {CONFIGS.keys()}.\nUsing default_config.")
+    return SimpleNamespace(**config)

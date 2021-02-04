f9f147a3ed8e99aca5cdec79d54d4be7d1e70b6e
Sebas <sebas.eliens@gmail.com>
2021-02-04 14:18:35 -0300

    sync and clean wandb after each run

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 python run.py --experiment 'bert-base'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'bert-large'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'roberta-base'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'roberta-large'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'albert-base'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'albert-large'
-wandb gc
+wandb sync --sync-all --clean
 python run.py --experiment 'distilbert-base-uncased'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'distilroberta'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'distilbert-base-squad2'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'roberta-base-squad2'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'distilroberta-base-squad2'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'bert-base-squad2'
-wandb sync --clean
+wandb sync --sync-all --clean
 python run.py --experiment 'albert-base-squad2'
-wandb sync --clean
+wandb sync --sync-all --clean

c423472c446a4107648f0c5c8ea703401323cc67
Sebas <sebas.eliens@gmail.com>
2021-02-04 11:32:05 -0300

    make sure to delete model with wandb gc

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 python run.py --experiment 'bert-base'
+wandb gc
 python run.py --experiment 'bert-large'
+wandb gc
 python run.py --experiment 'roberta-base'
+wandb gc
 python run.py --experiment 'roberta-large'
+wandb gc
 python run.py --experiment 'albert-base'
+wandb gc
 python run.py --experiment 'albert-large'
+wandb gc
 python run.py --experiment 'distilbert-base-uncased'
+wandb gc
 python run.py --experiment 'distilroberta'
+wandb gc
 python run.py --experiment 'distilbert-base-squad2'
+wandb gc
 python run.py --experiment 'roberta-base-squad2'
+wandb gc
 python run.py --experiment 'distilroberta-base-squad2'
+wandb gc
 python run.py --experiment 'bert-base-squad2'
+wandb gc
 python run.py --experiment 'albert-base-squad2'
+wandb gc

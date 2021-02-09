a188f6d329a7dcb01d7f68d7479d17f356aa715a
Sebas <sebas.eliens@gmail.com>
2021-02-08 22:52:20 -0300

    fix bash bug in run script

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-for model in
-bert-base
-bert-large
-roberta-base
-roberta-large
-albert-base
-albert-large
-distilbert-base
-distilroberta
-distilbert-base-squad2
-roberta-base-squad2
-distilroberta-base-squad2
-bert-base-squad2
-albert-base-squad2
+declare -a Models=(
+"bert-base"
+"bert-large"
+"roberta-base"
+"roberta-large"
+"albert-base"
+"albert-large"
+"distilbert-base"
+"distilroberta"
+"distilbert-base-squad2"
+"roberta-base-squad2"
+"distilroberta-base-squad2"
+"bert-base-squad2"
+"albert-base-squad2")
+for model in $Models
 do
     for g in 1 2 3
     do

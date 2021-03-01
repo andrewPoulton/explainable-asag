5089aeec8edc887ef6b47b8ff0eb0cd0b7b2d22c
Sebas <sebas.eliens@gmail.com>
2021-03-01 12:52:16 -0300

    config-ify token_types

---
+++ run_tokentypes.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+declare -a Models=(
+"bert-base"
+"bert-large"
+#"roberta-base"
+#"roberta-large"
+"albert-base"
+"albert-large"
+# "distilbert-base"
+# "distilroberta"
+# "distilbert-base-squad2"
+# "roberta-base-squad2"
+# "distilroberta-base-squad2"
+# "bert-base-squad2"
+# "albert-base-squad2"
+)
+
+for g in 1 2 3
+do
+    for model in "${Models[@]}"
+    do
+        python run.py "$model" "token_types" "$@" --group $g
+        wandb sync --sync-all
+        wandb sync --clean
+        rm -r wandb
+    done
+done

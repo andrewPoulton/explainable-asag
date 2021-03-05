9fa1cf36c3d296357a84c19efe84dcffb08defba
Sebas <sebas.eliens@gmail.com>
2021-03-05 16:37:26 -0300

    refactor evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -18,17 +18,29 @@ def get_annotations(annotation_dir):
         df = df.append({'name': name, 'source': source, 'origin':origin, 'annotation':annotation, 'question_id':question_id}, ignore_index = True)
     return df
 
-def word_idx(text, tokenizer, special_tokens = False):
+def list_word_token_idx(text, tokenizer, special_tokens = False):
     word_encodings =  [tokenizer.encode(word, add_special_tokens = special_tokens) for word in text.split()]
     it = iter(range(sum(len(w) for w in word_encodings)))
     word_idx = [[next(it) for j in w] for w in word_encodings]
     return word_idx
 
-def word_attr(attr, w_idx):
-    return [np.max(attr[w]) for w in w_idx]
+def compute_golden_saliency_vector(annotation_row, data_row):
+    return np.array([1 if f'word_{k+1}' in annotation_row['annotation'].split(',') else 0 for k in range(len( data_row['student_answers'].split()))])
 
-def saliency_score(attr):
-    return np.array(attr)/np.max(attr)
+def compute_student_answer_word_saliency_vector(attribution_row, data_row, tokenizer, aggr):
+    # First we select the attribibtuion corresponding to the student_answer_tokens (without [SEP])
+    question_tokens = tokenizer.encode(data_row['question_text'])
+    reference_tokens = tokenizer.encode(data_row['reference_answers'])
+    student_tokens = tokenizer.encode(data_row['student_answers'])
+    token_type_ids = [0] + [1]*(len(question_tokens)-1) + [2]*(len(reference_tokens)-1) + [3]*(len(student_tokens)-1)
+    attr = attribution_row['attr_' + aggr][[id ==3 for id in token_type_ids]][:-1]
+    # Then we map this to word attributions and normalize
+    # NOTE: choices below may be changed
+    w_idx = list_word_token_idx(data_row['student_answers'], tokenizer)
+    assert(len(attr) == sum(len(w) for w in w_idx))
+    attr = [np.max(attr[w]) for w in w_idx]
+    attr = np.array(attr)/np.max(attr)
+    return attr
 
 def compute_human_agreement(attribution_file, annotation_dir, aggr = 'norm'):
     name, source = os.path.split(attribution_file)[-1].split('_')[0:2]
@@ -47,24 +59,14 @@ def compute_human_agreement(attribution_file, annotation_dir, aggr = 'norm'):
     df.index = df['question_id']
     AP = [None]*len(df_anno)
     for i in range(len(df_anno)):
-        anno = df_anno.iloc[i]
-        data = df.loc[int(anno['question_id'])]
-        attr = df_attr.loc[int(anno['question_id'])]
-        # define the golden saliency from the annotation
-        golden = np.array([1 if f'word_{k+1}' in anno['annotation'].split(',') else 0 for k in range(len( data['student_answers'].split()))])
-        # next we define the saliency prediction from the attribution
-        question_tokens = tokenizer.encode(data['question_text'])
-        reference_tokens = tokenizer.encode(data['reference_answers'])
-        student_tokens = tokenizer.encode(data['student_answers'])
-        token_type_ids = [0] + [1]*(len(question_tokens)-1) + [2]*(len(reference_tokens)-1) + [3]*(len(student_tokens)-1)
-        st_attr = attr['attr_' + aggr][[id ==3 for id in token_type_ids]][:-1]
-        w_idx = word_idx(data['student_answers'], tokenizer)
-        assert(len(st_attr) == sum(len(w) for w in w_idx))
-        st_attr = word_attr(st_attr, w_idx)
-        saliency = saliency_score(st_attr)
+        annotation_row = df_anno.iloc[i]
+        data_row = df.loc[int(annotation_row['question_id'])]
+        attribution_row = df_attr.loc[int(annotation_row['question_id'])]
+        golden = compute_golden_saliency_vector(annotation_row, data_row)
+        saliency = compute_student_answer_word_saliency_vector(attribution_row, data_row, tokenizer, aggr)
         ap = average_precision_score(golden, saliency)
         AP[i] = ap
-        print('AP is ', ap)
+        #print('AP is ', ap)
 
     df_anno['AP'] = AP
     MAP = df_anno['AP'].mean()

145886b6c167ad9fbd5be617270c7543c1c1a2cc
Sebas <sebas.eliens@gmail.com>
2021-03-09 10:50:54 -0300

    normalize attr before selecting student answer

--- evaluation.py
+++ evaluation.py
@@ -10,15 +10,16 @@ DATA_FILE = 'data/flat_semeval5way_test.csv'
 
 def get_annotations(annotation_dir):
     df = pd.DataFrame(columns = ['name','source','origin','annotation', 'question_id'])
-    for ann in os.scandir(annotation_dir):
-        name, source, origin1, origin2, question_id = ann.name.split('.')[0].split('_')
+    for annotation_file in os.scandir(annotation_dir):
+        name, source, origin1, origin2, question_id = annotation_file.name.split('.')[0].split('_')
         origin = '_'.join([origin1,origin2])
-        with open(os.path.join(annotation_dir, ann.name), 'r') as f:
+        with open(os.path.join(annotation_dir, annotation_file.name), 'r') as f:
             annotation = f.readline()[:-1]
         df = df.append({'name': name, 'source': source, 'origin':origin, 'annotation':annotation, 'question_id':question_id}, ignore_index = True)
     return df
 
 def list_word_token_idx(text, tokenizer, special_tokens = False):
+    """Returns  list of form [[0,1], [2], [3,4]] where sublist corresponds to words in text and indices to the positions of the word-piece tokens after encoding"""
     word_encodings =  [tokenizer.encode(word, add_special_tokens = special_tokens) for word in text.split()]
     it = iter(range(sum(len(w) for w in word_encodings)))
     word_idx = [[next(it) for j in w] for w in word_encodings]
@@ -27,19 +28,27 @@ def list_word_token_idx(text, tokenizer, special_tokens = False):
 def compute_golden_saliency_vector(annotation_row, data_row):
     return np.array([1 if f'word_{k+1}' in annotation_row['annotation'].split(',') else 0 for k in range(len( data_row['student_answers'].split()))])
 
+def normalize(attr):
+    attr = np.array(attr)
+    attr = (attr - attr.min())/(attr.max() - attr.min())
+    return attr
+
 def compute_student_answer_word_saliency_vector(attribution_row, data_row, tokenizer, aggr):
-    # First we select the attribibtuion corresponding to the student_answer_tokens (without [SEP])
     question_tokens = tokenizer.encode(data_row['question_text'])
     reference_tokens = tokenizer.encode(data_row['reference_answers'])
     student_tokens = tokenizer.encode(data_row['student_answers'])
-    token_type_ids = [0] + [1]*(len(question_tokens)-1) + [2]*(len(reference_tokens)-1) + [3]*(len(student_tokens)-1)
-    attr = attribution_row['attr_' + aggr][[id ==3 for id in token_type_ids]][:-1]
-    # Then we map this to word attributions and normalize
-    # NOTE: choices below may be changed
+    start_student_tokens = len(question_tokens) + len(reference_tokens) - 2
+    end_student_tokens = len(question_tokens) + len(reference_tokens) + len(student_tokens) - 4
+    token_idx =range(start_student_tokens, end_student_tokens)
     w_idx = list_word_token_idx(data_row['student_answers'], tokenizer)
-    assert(len(attr) == sum(len(w) for w in w_idx))
+    print(len(token_idx), sum(len(w) for w in w_idx))
+    assert(len(token_idx) == sum(len(w) for w in w_idx))
+    # We get the attribitions corresponding to the aggregation method
+    attr = attribution_row['attr_' + aggr]
+    # Then we normalize, select student  (without [SEP]) answer and map to words
+    attr = normalize(attr)
+    attr = attr[token_idx]
     attr = [np.max(attr[w]) for w in w_idx]
-    attr = np.array(attr)/np.max(attr)
     return attr
 
 def compute_human_agreement(attribution_file, annotation_dir, aggr = 'norm'):

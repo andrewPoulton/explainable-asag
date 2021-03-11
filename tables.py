ecd7d23017e4b3d1c5318f6abb913bd79b259605
Sebas <sebas.eliens@gmail.com>
2021-03-10 21:10:23 -0300

    some code to pull data from wandb and make tables

---
+++ tables.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def f1_table(source):
+    df_wandb = pd.read_csv('tables/experiments.csv', index_col = 0)
+    if source == 'scientsbank':
+        group = 'group'
+    elif source =='beetle':
+        group = 'beetlebeetle'
+    else:
+        error('Only allowed data sources: scientsbank and beetle')
+
+    dfs = [df_wandb[df_wandb['group'] == group + j][['name', 'max_f1']].set_index('name') for j in ['-1','-2','-3']]
+    df = pd.concat(dfs, axis = 1)
+
+    df['mean'] = df.mean(axis = 1)
+    df['std'] = df.std(axis = 1)
+    return df
+
+
+
+
+if __name__=='__main__':
+    df = f1_table('scientsbank')
+    df.to_csv('tables/f1_scientsbank.csv')
+    with open('tables/f1_scientsbank.tex', 'w') as f:
+        f.write(df.to_latex())
+
+
+    df = f1_table('beetle')
+    df.to_csv('tables/f1_beetle.csv')
+    with open('tables/f1_beetle.tex', 'w') as f:
+        f.write(df.to_latex())

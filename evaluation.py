95afa5791667e85e9fc3b410180362a2c72040c1
Sebas <sebas.eliens@gmail.com>
2021-03-19 18:06:21 -0300

    define Annotations class

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 import numpy as np
 import configuration
@@ -10,145 +11,101 @@ from wandbinteraction import get_model_from_run_id
 from dataset import dataloader
 from scipy.stats import spearmanr
 from collections import defaultdict
-import re
 from functools import partial
 from itertools import chain, groupby
 from collections import Counter
 from dataset import SemEvalDataset, dataloader
 from sklearn.preprocessing import MinMaxScaler
+from dataset import __TEST_DATA__
+from tqdm import tqdm
 
-DATA_FILE = 'data/flat_semeval5way_test.csv'
-ATTRIBUTION_DIR = 'attributions'
-ANNOTATION_DIR = 'annotator/annotations'
+class AnnotationData:
+    def __init__(self, annotation_dir):
+        self.annotation_dir = annotation_dir
+        self.annotations = self.load_annotations()
+        self.annotator_names = self.annotations['annotator'].unique().tolist()
+        self.annotations['annotator_label'] = self.annotations['annotator'].replace({n:i+1 for i,n in enumerate(self.annotator_names)})
+        self.aggr_annotator = 'aggr'
+        self.aggr_function = lambda anns: [a for a, i in Counter(chain(anns)).items() if i > 1]
+        self.append_aggregated_annotations()
+        self._annotations = self.annotations.copy()
 
+    def load_annotations(self):
+        annotation_list = []
+        annotation_files = [f.name for f in os.scandir(self.annotation_dir) if f.name.endswith('annotation')]
+        for annotation_file_name in tqdm(annotation_files, desc=f"Loading annotations from {os.path.abspath(self.annotation_dir)}"):
+            annotator, source, origin1, origin2, instance_id = os.path.splitext(annotation_file_name)[0].split('_')
+            origin = '_'.join([origin1,origin2])
+            with open(os.path.join(self.annotation_dir, annotation_file_name), 'r') as f:
+                annotation = f.readline()[:-1]
+                annotation_list.append({'instance_id': instance_id,'source': source, 'origin': origin, 'annotator': annotator,  'annotation': annotation})
+        return pd.DataFrame.from_records(annotation_list)
 
-### Human Agreement
+    def append_aggregated_annotations(self):
+        for instance_id in tqdm(self.annotations['instance_id'].unique() , desc = 'Calculate aggregated annotations'):
+            _df = self.annotations[self.annotations['instance_id'] == instance_id]
+            _row = _df.iloc[0].copy()
+            _row['annotator'] = self.aggr_annotator
+            _row['annotation'] = self.aggr_function(_df['annotation'])
+            _row['annotator_label'] = 0
+            self.annotations = self.annotations.append(_row, ignore_index = True)
 
-def get_data_id_index(source):
-    if source =='beetle':
-        return range(468)
-    if source =='scientsbank':
-        return range(1315,1854)
-
-def get_model_path(model):
-    model_path = load_configs_from_file(os.path.join('configs', 'models.yml'))[model]['model_name']
-    return model_path
-
-def get_tokenizer(model):
-    model_path = get_model_path(model)
-    tokenizer =  transformers.AutoTokenizer.from_pretrained(model_path, lowercase=True)
-    return tokenizer
-
-def read_annotation(annotation_dir, annotation_file_name):
-    annotator, source, origin1, origin2, data_id = annotation_file_name.split('.')[0].split('_')
-    origin = '_'.join([origin1,origin2])
-    with open(os.path.join(annotation_dir, annotation_file_name), 'r') as f:
-        annotation = f.readline()[:-1]
-    return {'annotator': annotator, 'source': source, 'origin': origin, 'data_id': data_id, 'annotation': annotation}
-
-def get_annotations(annotation_dir):
-    df = pd.DataFrame(columns = ['annotator','source','origin','annotation', 'data_id'])
-    for annotation_file in os.scandir(annotation_dir):
-        annotation_row = read_annotation(annotation_dir, annotation_file.name)
-        df = df.append(annotation_row, ignore_index = True)
-    return df
-
-def aggregate_annotators(df_annotations):
-    ann_aggr = lambda anns: [a for a, i in Counter(chain(anns)).items() if i > 1]
-    df = pd.DataFrame(columns = df_annotations.columns)
-    for data_id in df_annotations['data_id'].unique():
-        _df = df_annotations[df_annotations['data_id'] == data_id]
-        _row = _df.iloc[0]
-        _row['annotator'] = 'aggr'
-        _row['annotation'] = ann_aggr(_df['annotation'])
-        df = df.append(_row, ignore_index = True)
-    return df
-
-def read_attribution(attribution_dir, attribution_file_name, attr_is_pred = False):
-    print('Read attribution file', attribution_file_name)
-    model, source, run_id, attribution_method = attribution_file_name.split('.')[0].split('_')
-    df = pd.read_pickle(os.path.join(attribution_dir,attribution_file_name))
-    if attr_is_pred:
-        df = df[df['attr_class']== df['pred']]
-        df['data_id'] = get_data_id_index(source)
-    else:
-        df['data_id'] = [i for i in get_data_id_index(source) for _ in range(df['attr_class'].max())]
-    return {'df': df, 'model': model, 'source': source, 'run_id': run_id, 'attribution_method' : attribution_method}
+    def get_golden_saliency(self, annotator, instance_id, data):
+        # should throw error if annotator has not annotated specific instance
+        from_annotator =  self.annotations[self.annotations['annotator'] == annotator].set_index('instance_id')
+        annotation = from_annotator.loc[instance_id]
+        student_answer = data.get_instance(instance_id)['student_answers']
+        return [1 if f'word_{k}' in annotation.split(',')
+                                else 0 for k,w in student_answer.split())])
 
 
-def list_word_token_idx(text, tokenizer, special_tokens = False):
-    """Returns  list of form [[0,1], [2], [3,4]] where sublist corresponds to words in text and indices to the positions of the word-piece tokens after encoding"""
-    word_encodings =  [tokenizer.encode(word, add_special_tokens = special_tokens) for word in text.split()]
-    b = tokenizer.encode_plus(text, add_special_tokens = special_tokens)
-    c = [b.char_to_token(j) for j in range(len(text))] + [None]
-    spc_pos = [i for i,t in enumerate(c) if t == None]
-    word_idx = [list(set(c[i+1:j])) for i,j in zip([-1]+spc_pos, spc_pos)]
-    return word_idx
+
+# def read_attribution(attribution_dir, attribution_file_name, attr_is_pred = False):
+#     print('Read attribution file', attribution_file_name)
+#     model, source, run_id, attribution_method = attribution_file_name.split('.')[:-1].split('_')
+#     df = pd.read_pickle(os.path.join(attribution_dir,attribution_file_name))
+#     if attr_is_pred:
+#         df = df[df['attr_class']== df['pred']]
+#         df['instance_id'] = get_instance_id_index(source)
+#     else:
+#         df['instance_id'] = [i for i in get_instance_id_index(source) for _ in range(df['attr_class'].max())]
+#     return {'df': df, 'model': model, 'source': source, 'run_id': run_id, 'attribution_method' : attribution_method}
+
+
+# def list_word_token_idx(text, tokenizer, special_tokens = False):
+#     """Returns  list of form [[0,1], [2], [3,4]] where sublist corresponds to words in text and indices to the positions of the word-piece tokens after encoding"""
+#     word_encodings =  [tokenizer.encode(word, add_special_tokens = special_tokens) for word in text.split()]
+#     b = tokenizer.encode_plus(text, add_special_tokens = special_tokens)
+#     c = [b.char_to_token(j) for j in range(len(text))] + [None]
+#     spc_pos = [i for i,t in enumerate(c) if t == None]
+#     word_idx = [list(set(c[i+1:j])) for i,j in zip([-1]+spc_pos, spc_pos)]
+#     return word_idx
 
 def scale_to_unit_interval(attr):
-    return torch.Tensor(MinMaxScaler().fit_transform(attr.unsqueeze(0)).squeeze(0)).long()
+    return torch.Tensor(MinMaxScaler().fit_transform(torch.Tensor(attr).unsqueeze(0)).squeeze(0)).long()
 
+def compute_golden_saliency(annotation, sentence):
+    return [1 if f'word_{k}' in annotation.split(',') else 0 for k,w in enumerate(sentence.split())]
 
-def compute_golden_saliency_vector(annotation, sentence):
-    return np.array([1 if f'word_{k}' in annotation.split(',') else 0 for k in range(len( sentence.split()))])
-
-def compute_student_answer_word_saliency_vector(attr, data_row, tokenizer):
+def compute_attribution_saliency(attr, data):
     # We need to know the indices for student answer tokens and how they group as words
-    _attr = torch.Tensor(attr).long()
-    _attr = scale_to_unit_interval(_attr)
-    assert _attr.shape == data_row['token_types'].shape, 'Attribution not same shape as encoded data.'
-    _attr = torch.masked_select(_attr, data_row['token_types'].eq(3))
-    _attr = _attr.index_select(0, torch.arange(0, _attr.size(0) - 1, dtype = int))
-    w_idx = list_word_token_idx(data_row['student_answers'], tokenizer)
-    assert _attr.size(0) == max(list(chain(*w_idx)) )+1, 'Student tokens to words mismatch'
-    _attr = [np.max(_attr.numpy()[w]) for w in w_idx]
-    return _attr
+    row = data.get_instance(attr['instance'])
+    return _attr.tolist()
 
 
-def get_testdata():
-    df = pd.read_csv(DATA_FILE)
-    df['data_id'] = df.index
-    return df
-
-def get_testdataset(model, source):
-    dataset = SemEvalDataset(data_file = DATA_FILE, vocab_file = get_model_path(model))
-    dataset.set_data_source(source)
-    dataset.to_val_mode(source, 'answer')
-    print(f"Test dataset loaded from {DATA_FILE} with {dataset.data.shape[0]} lines.")
-    return dataset
-
-def get_testdataloader(model, source):
-    if torch.cuda.is_available():
-        num_workers = 8
-    else:
-        num_workers = 0
-    loader = dataloader(
-        val_mode = True,
-        data_file = DATA_FILE,
-        data_source = source,
-        vocab_file = get_model_path(model),
-        num_labels = 2,
-        train_percent = 100,
-        batch_size = 1,
-        drop_last = False,
-        num_workers = num_workers)
-    return loader
-
-
-def compute_human_agreement(df_annotations, attribution_file_name, aggr = 'L2'):
+def compute_human_agreement(dataset, attribution_df, aggr):
     print('Computing human agreement (HA) for', attribution_file_name)
     attribution = read_attribution(ATTRIBUTION_DIR, attribution_file_name, attr_is_pred = True)
-    loader = get_testdataloader(attribution['model'], attribution['source'])
-    dataset = loader.dataset
+    dataset = get_testdataset(attribution['model'], attribution['source'])
     tokenizer = dataset.tokenizer
     df_anno = df_annotations[df_annotations['source'] == attribution['source']]
-    df_attr = attribution['df'].set_index('data_id')
+    df_attr = attribution['df'].set_index('instance_id')
     AP = [0.0]*len(df_anno)
     for i in range(len(df_anno)):
         annotation_row = df_anno.iloc[i]
-        data_id = int(annotation_row['data_id'])
-        data_row = dataset.get_row(data_id)
-        attribution_row = df_attr.loc[data_id]
+        instance_id = int(annotation_row['instance_id'])
+        data_row = dataset.get_row(instance_id)
+        attribution_row = df_attr.loc[instance_id]
         golden = compute_golden_saliency_vector(annotation_row['annotation'], data_row['student_answers'])
         saliency = compute_student_answer_word_saliency_vector(attribution_row['attr_' + aggr], data_row, tokenizer)
         ap = average_precision_score(golden, saliency)
@@ -156,11 +113,12 @@ def compute_human_agreement(df_annotations, attribution_file_name, aggr = 'L2'):
 
     MAP = np.mean(AP)
     return {'attribution_file_name': attribution_file_name,
-            'HA': MAP,
+            'metric': MAP,
             'run_id': attribution['run_id'],
             'model': attribution['model'],
             'source': attribution['source'],
-            'attribution_method' : attribution['attribution_method']}
+            'attribution_method' : attribution['attribution_method'],
+            'aggr': aggr}
 
 
 
@@ -214,7 +172,7 @@ def compute_diff_attribution(attr1, attr2):
     return np.mean(np.abs(np.array(attr1) -np.array(attr2)))
 
 
-def compute_rationale_consistency(attribution_file1, attribution_file2, aggr = 'L2', **kwargs):
+def compute_rationale_consistency(attribution_file1, attribution_file2, aggr, **kwargs):
     A1 = read_attribution(ATTRIBUTION_DIR, attribution_file1, attr_is_pred=True)
     A2 = read_attribution(ATTRIBUTION_DIR, attribution_file2, attr_is_pred=True)
     assert A1['model'] == A2['model'] and A1['source'] == A2['source'] and A1['attribution_method'] == A2['attribution_method']
@@ -245,4 +203,12 @@ def compute_rationale_consistency(attribution_file1, attribution_file2, aggr = '
         diff_activation[i] = compute_diff_activation(model1, model2, batch)
         diff_attribution[i] = compute_diff_attribution(attr1, attr2)
     r = spearmanr(diff_activation, diff_attribution)
-    return r
+    return {'attribution_file_name1': attribution_file1.name,
+            'attribution_file_name2': attribution_file2.name,
+            'run_id1': A1['run_id'],
+            'run_id2': A2['run_id'],
+            'metric': r,
+            'model': A1['model'],
+            'source': A1['source'],
+            'attribution_method' : A1['attribution_method'],
+            'aggr': aggr}

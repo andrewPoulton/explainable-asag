7ac8f11aa411ff5c070a8e23423c03dd1f7e0b49
Sebas <sebas.eliens@gmail.com>
2021-03-26 19:27:23 -0300

    don't complain if backup dirs already exist

--- backup_attr_to_json.py
+++ backup_attr_to_json.py
@@ -3,7 +3,7 @@ from evaluation import AttributionData
 print('Copying attribution pickle to json.')
 for dirpath, dirs, files in os.walk("attributions"):
     for d in dirs:
-        os.makedirs(os.path.join('attributions_json', d))
+        os.makedirs(os.path.join('attributions_json', d), exist_ok= True)
         for f in os.listdir(os.path.join(dirpath, d)):
             if f.endswith('.pkl'):
                 pickle_file = os.path.join(dirpath, d, f)

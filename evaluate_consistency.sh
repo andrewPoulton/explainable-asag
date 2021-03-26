c2059ab92ff7c3d7a2677bff99fc2c4361f102d9
Sebas <sebas.eliens@gmail.com>
2021-03-26 20:29:15 -0300

    uncomment lines so script runs all RC and DC

--- evaluate_consistency.sh
+++ evaluate_consistency.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 python evaluateDC.py attributions/beetle-1
 python evaluateDC.py attributions/scientsbank-1
-#python evaluateDC.py attributions/beetle-token_types-1
-#python evaluateDC.py attributions/scientsbank-token_types-1
+python evaluateDC.py attributions/beetle-token_types-1
+python evaluateDC.py attributions/scientsbank-token_types-1
 
 python evaluateRC.py attributions/beetle-1 attributions/beetle-2
 python evaluateRC.py attributions/scientsbank-1 attributions/scientsbank-2

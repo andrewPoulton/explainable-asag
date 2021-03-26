42e600ead11d213138a4a28b2d0f95501ba8d6d9
Sebas <sebas.eliens@gmail.com>
2021-03-26 19:24:52 -0300

    try something saver first...

--- backup_attr_to_json.py
+++ backup_attr_to_json.py
@@ -1,10 +1,14 @@
 import os
 from evaluation import AttributionData
+print('Copying attribution pickle to json.')
 for dirpath, dirs, files in os.walk("attributions"):
     for d in dirs:
         os.makedirs(os.path.join('attributions_json', d))
         for f in os.listdir(dirpath, d):
             if f.endswith('.pkl'):
-                a = AttributionData(os.path.join(dirpath, d, f))
-                a.to_json(os.path.join('attributions_json', d, os.path.splitext(f)[0]+'.json'))
-))
+                pickle_file = os.path.join(dirpath, d, f)
+                json_file = os.path.join('attributions_json', d, os.path.splitext(f)[0]+'.json')
+                print('pickle:', _file)
+                print('json:', json_file)
+#                 a = AttributionData(pickle_file)
+#                 a.to_json(json_file)

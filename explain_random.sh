811ec1bde4589a7a8e29ad7c01153d07c5296259
Sebas <sebas.eliens@gmail.com>
2021-03-28 07:33:16 -0300

    prepare attribution runs for random init models

---
+++ explain_random.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+python explainrandom.py scientsbank bert-base
+python explainrandom.py scientsbank bert-large
+python explainrandom.py scientsbank albert-base
+python explainrandom.py scientsbank albert-large
+python explainrandom.py scientsbank distilbert-base
+
+python explainrandom.py beetle bert-base
+python explainrandom.py beetle bert-large
+python explainrandom.py beetle albert-base
+python explainrandom.py beetle albert-large
+python explainrandom.py beetle distilbert-base
---
+++ explainrandom.py
@@ -0,0 +1,56 @@
+import fire
+import os
+import pandas as pd
+import torch
+from configuration import load_configs_from_file
+from dataset import dataloader, __TEST_DATA__
+from explanation import explain_model
+from wandbinteraction import get_runs, load_model_from_run
+import transformers
+
+__CUDA__ = torch.cuda.is_available()
+__EXPLANATIONS_DIR__ = 'explained'
+__num_workers__ = 4
+
+def explain(source, name, token_types = False, origin = 'unseen_answers'):
+    num_labels = 2
+    group =  source + ('-token_types' if token_types else '')
+    attr_directory = os.path.join(__EXPLANATIONS_DIR__, 'random_'+ group)
+    os.makedirs(attr_directory, exist_ok=True)
+    path_to_attribution_file = os.path.join(attr_directory, model + '.pkl')
+    model_path = load_configs_from_file(os.path.join('configs', 'main.yml'))[name]['model_name']
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path, num_labels = num_labels)
+    config = {
+        'token_types' = token_types,
+        'num_labels' = num_labels
+    }
+    model.init_weights()
+    loader = dataloader(
+        data_file = __TEST_DATA__,
+        val_mode = True,
+        data_source = source,
+        data_val_origin = origin,
+        vocab_file = model_path,
+        num_labels = num_labels,
+        train_percent = 100,
+        batch_size = 1,
+        drop_last = False,
+        num_workers = __num_workers__ if __CUDA__ else 0)
+    attr_configs = load_configs_from_file(os.path.join('configs','explain.yml'))['EXPLAIN']
+    if 'large' in name:
+        for attribution_method in attr_configs.keys():
+            if attr_configs[attribution_method] and 'internal_batch_size' in attr_configs[attribution_method]:
+                attr_configs[attribution_method]['internal_batch_size'] = attr_configs[attribution_method]['internal_batch_size']//2
+    df = explain_model(loader, model, config,  attr_configs, origin, __CUDA__)
+    df['run_id'] = 'random'
+    df['model'] = name
+    df['model_path'] = model_name
+    df['source'] = source
+    df['origin'] = origin
+    df['num_labels'] = num_labels
+    df['group'] = group
+    df['token_types'] = token_types
+    df.to_pickle(path_to_attribution_file)
+
+if __name__=='__main__':
+    fire.Fire(explain)

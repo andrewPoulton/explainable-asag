61fd42e161381183edd1ea48e9969a41b22c9926
Sebas <sebas.eliens@gmail.com>
2021-04-01 11:10:31 -0300

    remove dataset_consistency import

--- evaluateRC.py
+++ evaluateRC.py
@@ -5,8 +5,7 @@ from evaluation import (
     AnnotationData,
     AttributionData,
     compute_human_agreement,
-    compute_rationale_consistency,
-    compute_dataset_consistency,
+    compute_rationale_consistency
     __RESULTS_DIR__
 )
 import torch
